583baf43624511f78f265047da8eca5a46439ffc
KnockYouth <59814509@qq.com>
2018-02-28 19:17:55 +0800

    获取所有应用

---
+++ platforms/android/app/src/main/java/com/qingyi/applocker/filter/AppsFilter.kt
@@ -0,0 +1,15 @@
+package com.qingyi.applocker.filter
+
+/**
+ * @Title: AppsFilter类
+ * @Package: com.qingyi.applocker.filter
+ * @Description: 应用过滤器(不需要显示的应用)
+ * @author XueLong xuelongqy@foxmail.com
+ * @date 2018/2/28 10:25
+ * @update_author
+ * @update_time
+ * @version V1.0
+ * @exception
+*/
+class AppsFilter {
+}
