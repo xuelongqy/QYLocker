e1818bedb24e704dc91b9009c63aa9c97343dbd8
KnockYouth <59814509@qq.com>
2018-04-26 16:53:10 +0800

    添加主题存储

--- platforms/android/app/src/main/java/com/qingyi/applocker/cordova/plugin/ThemePlugin.kt
+++ platforms/android/app/src/main/java/com/qingyi/applocker/cordova/plugin/ThemePlugin.kt
@@ -55,7 +55,7 @@ class ThemePlugin: CordovaPlugin() {
                 return true
             }
             "getThemeList" -> {
-
+                return true
             }
         }
         return false
